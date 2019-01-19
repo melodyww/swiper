94004a01368b20bc868078fb85ad7e81f7fba2ad
melodyww <yangjieuidesign@163.com>
2019-01-19 11:53:26 +0800

    修改个人配置

--- user/api.py
+++ user/api.py
@@ -8,6 +8,7 @@ from lib.sms import send_sms
 
 from common import keys
 from common import errors
+from user.forms import ProfileForm
 from user.models import User
 
 
@@ -35,15 +36,22 @@ def submit_vcode(request):
 # @require_GET
 # @require_http_methods(['GET','POST'])
 def get_profile(request):
-    """获取个人配置"""
+    """获取个人资料"""
     # if request.method == 'GET':
     uid = request.session['uid']
     user = User.objects.get(id=uid)
     return render_json(user.profile.to_dict())
 
 def set_profile(request):
-    """设置个人资料"""
-    return None
+    """修改个人配置"""
+    form = ProfileForm(request.POST)
+    if form.is_valid():
+        profile = form.save(commit=False)
+        profile.id = request.session["uid"]
+        profile.save()
+        return render_json(None)
+    else:
+        return render_json(form.errors, errors.PROFILE_ERR)
 
 def upload_avatar(request):
     """上传头像"""
